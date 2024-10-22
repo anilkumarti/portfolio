12068c849d2c141dfee191b6285f6d2400c8b3e4
Anil Kumar Tiwari <anil.tiwari1403@gmail.com>
2024-10-22 23:26:34 +0530

    chat app project added

--- src/compoment/Contact/Contact.jsx
+++ src/compoment/Contact/Contact.jsx
@@ -1,28 +1,98 @@
-import React from 'react'
-import Styles from './ContactStyles.module.css'
+import { useState } from "react";
+import emailjs from "@emailjs/browser";
+
+import Styles from "./ContactStyles.module.css";
+import { toast } from "react-toastify";
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formattedData = {
+      from_name: formData.name,
+      from_email: formData.email,
+      message: formData.message,
+      to_name: "Anil Kumar Tiwari",
+    };
+    console.log(formattedData)
+    emailjs
+      .send(
+        "service_pn0hejj",
+        "template_a1mj99d",
+        formattedData,
+        "0j8lNORax-WJ9pPoR"
+      )
+      .then((res) => {
+        console.log("Message sent successfully", res.status);
+        toast.success("Message sent successfully");
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.warning(error);
+      });
+    setFormData({ name: "", email: "", message: "" });
+  };
   return (
-    <section id='contact' className={Styles.container}>
-   <h1 className='sectionTitle'> Contact</h1>
-   <form action="">
-    <div className="formGroup">
-        <label htmlFor="name" hidden> Name</label>
-    <input type="text" name='name' id='name' placeholder='name' required />
-    </div>
+    <section id="contact" className={Styles.container}>
+      <h1 className="sectionTitle"> Contact</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="formGroup">
+          <label htmlFor="name" hidden>
+            Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="name"
+            onChange={handleChange}
+            value={formData.name}
+            required
+          />
+        </div>
 
-    <div className="formGroup">
-        <label htmlFor="email" hidden> Email</label>
-    <input type="email" name='email' id='email' placeholder='Email' required />
-    </div>
-    <div className="formGroup">
-        <label htmlFor="message" hidden> Message</label>
-    <textarea type="text" name='message' id='message' placeholder='message' required />
-    </div>
-    <input className='hover btn' type="submit"  submit value="Submit"/>
-   </form>
+        <div className="formGroup">
+          <label htmlFor="email" hidden>
+            {" "}
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email"
+            onChange={handleChange}
+            value={formData.email}
+            required
+          />
+        </div>
+        <div className="formGroup">
+          <label htmlFor="message" hidden>
+            {" "}
+            Message
+          </label>
+          <textarea
+            type="text"
+            name="message"
+            id="message"
+            placeholder="message"
+            value={formData.message}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <input type="submit" value='Send'/>
+      </form>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
--- src/compoment/Projects/Projects.jsx
+++ src/compoment/Projects/Projects.jsx
@@ -4,11 +4,14 @@ import viber from '../../assets/viberr.png'
 import freshBurger from '../../assets/fresh-burger.png'
 import alphaStore from '../../assets/alpha-store.jpg'
 import homzy from '../../assets/homzy.jpg'
+import chatapp from '../../assets/chatapp.png'
 import ProjectCard from '../../Common/ProjectCard'
 const Projects = () => {
   return (
     <section id='projects' className={styles.container}>
    <div className={styles.projectsContainer}> 
+   <ProjectCard src={chatapp} link={'https://chatter-box-eight.vercel.app/'} h3="DeskChat" p="Desktop messaging for work and personal chat." />
+
         <ProjectCard src={homzy} link={'https://reale-state-nine.vercel.app/'} h3="Homzy" p="Real Estate website" />
 
         <ProjectCard src={alphaStore} link={'https://ecommerce-site-shop.vercel.app/'} h3="Alpha-store" p="E-commerce website" />
